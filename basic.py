8dffb7bbeb21be9a2c290cab18615bafe3175ae2
Kevin Li <likevintw@gmail.com>
2022-03-30 22:42:35 +0800

    first commit

---
+++ basic.py
@@ -0,0 +1,99 @@
+
+
+def get_reminder(dividend, divisor):
+    '''
+    %
+    remider
+    (5,3)=2
+    '''
+    return dividend % divisor
+
+
+def show_xor_example(x, y):
+    '''
+    x^0=x
+    x^x=0
+    '''
+    return x ^ y
+
+
+class PropertyExampler:
+    '''
+    encapsulation
+    property
+    '''
+
+    def __init__(self, email):
+        self.__email = email
+
+    @property
+    def email_special_api(self):
+        return self.__email
+
+    @email_special_api.setter
+    def email_special_api(self, value):
+        self.__email = value
+
+    @email_special_api.getter
+    def email_special_api(self):
+        return self.__email
+
+    @email_special_api.deleter
+    def email_special_api(self):
+        del self.__email
+
+
+class CheckDataExampler:
+    '''
+    dynamic check attribute
+    '''
+    __id: int
+
+    def __init__(self, id, is_valid_id) -> None:
+        self.id = id
+        self.is_valid_id = is_valid_id
+
+    def update_id(self, new_id):
+        if self.is_valid_id(new_id):
+            self.id = new_id
+            return True
+        return False
+
+    @staticmethod
+    def check_id(id):
+        if len(id) < 6:
+            return False
+        return True
+
+
+class EnterExitExampler:
+    def __init__(self) -> None:
+        print("__init__")
+
+    def __enter__(self):
+        print("__enter__")
+
+    def __exit__(self, type, value, traceback):
+        print("__exit__")
+
+
+class CallExampler:
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self) -> None:
+        return True
+
+class ItemExampler:
+    def __init__(self, key, value):
+        self.classmate = {}
+        self.classmate[key] = value
+
+    def __getitem__(self, key):
+        return self.classmate[key]
+
+    def __setitem__(self, key, value):
+        self.classmate[key] = value
+
+    def __delitem__(self):
+        del self.classmate
---
+++ test_basic.py
@@ -0,0 +1,88 @@
+import unittest
+import basic
+
+unittest.TestCase.shortDescription = lambda x: None  # don't show docstring
+
+
+class TestBasic(unittest.TestCase):
+    def test_get_reminder(self):
+        class TestFormat:
+            def __init__(self, args, wants) -> None:
+                self.args = args
+                self.wants = wants
+        cases = []
+        cases.append(TestFormat([5, 3], 2))
+        cases.append(TestFormat([15, 3], 0))
+        cases.append(TestFormat([52, 3], 1))
+        cases.append(TestFormat([535, 7], 3))
+
+        for case in cases:
+            result = basic.get_reminder(case.args[0], case.args[1])
+            self.assertEqual(result, case.wants)
+
+    def test_show_xor_example(self):
+        class TestFormat:
+            def __init__(self, args, wants) -> None:
+                self.args = args
+                self.wants = wants
+        cases = []
+        cases.append(TestFormat([3, 0], 3))
+        cases.append(TestFormat([3, 3], 0))
+        cases.append(TestFormat([15, 3], 12))
+        cases.append(TestFormat([52, 3], 55))
+        cases.append(TestFormat([535, 7], 528))
+
+        for case in cases:
+            result = basic.show_xor_example(case.args[0], case.args[1])
+            self.assertEqual(result, case.wants)
+
+    def test_show_docstring(self):
+        '''
+        Function: basic.get_reminder.__doc__
+        Mehtod: basic.People.__doc__
+        '''
+        pass
+
+    def test_show_xor_example(self):
+        '''
+        class property - encapsulation
+        '''
+        handler = basic.PropertyExampler('abc@yahoo.com')
+        self.assertEqual(handler.email_special_api, 'abc@yahoo.com')
+        handler.email_special_api = 'hello@world.com'
+        self.assertEqual(handler.email_special_api, 'hello@world.com')
+        del handler.email_special_api
+        try:
+            handler.email_special_api
+        except:
+            self.assertTrue(True)
+
+    def test_dynamic_check_id(self):
+        '''
+        dynamic check attribute
+        extension management
+        '''
+        handler = basic.CheckDataExampler(
+            '123456', basic.CheckDataExampler.check_id)
+        self.assertEqual(handler.update_id('123'), False)
+        self.assertEqual(handler.update_id('666666'), True)
+
+    def test_exnter_and_exit_example(self):
+        """
+        __enter__ and __exit__
+        with >> enter >> do something >> exit
+        """
+
+        with basic.EnterExitExampler() as handler:
+            print("do_something")
+
+    def test_call_example(self):
+        """
+        __call__ and __exit__
+        """
+        handler = basic.CallExampler()
+        self.assertEqual(handler(), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
